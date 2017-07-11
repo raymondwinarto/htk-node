35e9458aef9d924b8e6d1cc938b9956c95581336
Raymond <rays_win@hotmail.com>
2017-07-11 22:29:23 +1000

    adding post that reads request body

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hometrack Test Application, please send a POST request');
+})
+
+app.post('/', function(request, response) {
+  var responseString = '';
+  responseString = request.body;
+
+
+
+  response.send(responseString);
 })
 
 app.listen(app.get('port'), function() {
